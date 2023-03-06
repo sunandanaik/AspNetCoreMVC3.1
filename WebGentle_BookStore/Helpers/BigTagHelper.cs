a472f3db77c0ec7507f0e58b99e5eccb2f9f3ce8
sunandanaik <sungitmca@gmail.com>
2023-03-06 18:14:49 +0530

    Created custom tag helper in asp.net core.

---
+++ WebGentle_BookStore/Helpers/BigTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebGentle_BookStore.Helpers
+{
+    //In order to target html tag
+    [HtmlTargetElement("big", Attributes = "big")]
+    //[HtmlTargetElement(Attributes= "big")]
+    public class BigTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "h3";
+            output.Attributes.RemoveAll("big");
+            output.Attributes.SetAttribute("class", "h3");
+            //base.Process(context, output);
+        }
+    }
+}
---
+++ WebGentle_BookStore/Helpers/CustomEmailTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebGentle_BookStore.Helpers
+{
+    public class CustomEmailTagHelper : TagHelper
+    {
+        //To dynamically pass value
+        public string MyEmail { get; set; } //this will be treated as attribute in the tag.
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a"; //for anchor tag <a>
+            //Now to define attributes to tag helper
+            output.Attributes.SetAttribute("href", "mailto:sungitmca@gmail.com");
+            //OR
+            output.Attributes.SetAttribute("href", $"mailto:{MyEmail}");
+            //OR
+            output.Attributes.Add("id", "my-email-id");
+            //To Set content
+            output.Content.SetContent("my-email");
+            //base.Process(context, output);
+        }
+    }
+}
